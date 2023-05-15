3bbfb2f506431cdbf19e6c80ce0446f36bfb8628
Tomoki Sugiura <sugiuratomoki7@gmail.com>
2023-05-15 14:24:56 +0900

    add program and dataset for 4.7.1 problem

--- tensor_from_images.py
+++ tensor_from_images.py
@@ -8,11 +8,14 @@ if __name__ == '__main__':
     image_names = [image_name for image_name in os.listdir(image_dir)
                    if os.path.splitext(image_name)[-1] == '.bmp']
     batch_size = len(image_names)
-    batch = torch.zeros(batch_size, 3, 256, 256, dtype=torch.uint8)
+    batch = torch.zeros(batch_size, 3, 256, 256)
     for i, image_name in enumerate(image_names):
         image_array = imageio.imread(os.path.join(image_dir, image_name))
         image_tensor = torch.tensor(image_array)
         image_tensor = image_tensor.permute(2, 0, 1)
         image_tensor = image_tensor[:3]
         batch[i] = image_tensor
-    print(batch)
+    brightness = batch.mean(dim=(1, 2, 3))
+    rgb_brightness = batch.mean(dim=(2, 3))
+    for i, image_name in enumerate(image_names):
+        print(f'{i}: {image_name}, {rgb_brightness[i]}')
