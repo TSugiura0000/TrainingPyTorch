676a96abc317c72202d4988348670a2d2554a18b
TSugiura0000 <tsugiura@mis.doshisha.ac.jp>
2023-06-15 11:20:28 +0900

    add Discriminator

--- gan.py
+++ gan.py
@@ -39,19 +39,110 @@ def create_mnist_dataloader(n_batch: int = 256):
 
 
 class Discriminator(nn.Module):
-    def __init__(self) -> None:
+    def __init__(
+            self,
+            input_dim: tuple = (1, 28, 28),
+            conv_kernel_size: None or list = None,
+            conv_kernel_filter: None or list = None,
+            dropout_rate: float = 0.0,
+            activation: str = 'relu'
+    ) -> None:
+        """Discriminator
+        The number of elements in the list
+        (conv_kernel_size, conv_kernel_filter) must be 4.
+        """
         super().__init__()
+        dropout_layer = nn.Dropout2d(p=dropout_rate)
+        activate_fn = nn.ReLU()
+        if conv_kernel_size is None:
+            conv_kernel_size = [3, 3, 3, 3]
+        if conv_kernel_filter is None:
+            conv_kernel_filter = [64, 64, 128, 128]
+        if activation == 'leaky_relu':
+            activate_fn = nn.LeakyReLU(inplace=True)
+        self._input_dim = input_dim
+        self._conv_kernel_size = conv_kernel_size
+        self._conv_kernel_filter = conv_kernel_filter
+        self._dropout = dropout_layer
+        self._activate_fn = activate_fn
+        self._conv_layers = nn.ModuleList(
+            [nn.Conv2d(
+                in_channels=in_chan,
+                out_channels=out_chan,
+                kernel_size=k_size,
+                stride=1,
+                padding=1
+            )
+                for in_chan, out_chan, k_size
+                in zip(
+                [self._input_dim[0]] + self._conv_kernel_filter[0:3],
+                self._conv_kernel_filter,
+                self._conv_kernel_size
+            )]
+        )
+        self._last_linear = nn.Linear(
+            ((self._input_dim[1] // 8) ** 2) * conv_kernel_filter[-1],
+            1
+        )
 
-    def forward(self):
-        pass
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for i, conv in enumerate(self._conv_layers):
+            x = self._activate_fn(conv(x))
+            x = self._dropout(x)
+            if i != len(self._conv_layers):
+                x = F.max_pool2d(x, 2)
+        x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])
+        x = self._last_linear(x)
+        x = F.sigmoid(x)
+        return x
 
 
 class Generator(nn.Module):
-    def __init__(self) -> None:
+    def __init__(
+            self,
+            z_dim: int = 100,
+            init_linear_size: tuple = (7, 7, 64),
+            conv_kernel_size: None or list = None,
+            conv_kernel_filter: None or list = None,
+            dropout_rate: float = 0.0,
+            batch_norm: bool = True,
+            activation: str = 'relu',
+            output_dim: tuple = (1, 28, 28)
+    ) -> None:
+        """Discriminator
+        The number of elements in the list
+        (conv_kernel_size, conv_kernel_filter) must be 4.
+        """
         super().__init__()
+        batch_norm_layers = []
+        dropout_layer = nn.Dropout2d(p=dropout_rate)
+        activate_fn = nn.ReLU()
+        init_linear_features = \
+            init_linear_size[0] * init_linear_size[1] * init_linear_size[2]
+        if conv_kernel_size is None:
+            conv_kernel_size = [3, 3, 3, 3]
+        if conv_kernel_filter is None:
+            conv_kernel_filter = [128, 64, 64, 1]
+        if activation == 'leaky_relu':
+            activate_fn = nn.LeakyReLU(inplace=True)
+        if batch_norm:
+            batch_norm_layers.append(nn.BatchNorm1d(init_linear_features))
+            for features in conv_kernel_filter[0:-1]:
+                batch_norm_layers.append(nn.BatchNorm2d(features))
+        else:
+            for _ in range(4):
+                batch_norm_layers.append(nn.Identity())
 
-    def forward(self):
-        pass
+        self._init_linear = nn.Linear(z_dim, init_linear_features)
+        self._nn_batch_norm_layers = nn.ModuleList(batch_norm_layers)
+        self._dropout = dropout_layer
+        self._activate_fn = activate_fn
+
+    def forward(self, z: torch.Tensor) -> torch.Tensor:
+        x = self._init_linear(z)
+        x = self._nn_batch_norm_layers[0](x)
+
+        return x
 
 
 if __name__ == '__main__':
